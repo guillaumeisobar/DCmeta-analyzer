52c01b66016f2d0a8b502bd2f8ed861f9ac812a5
Guillaume Olivieri <31344086+guillaumeisobar@users.noreply.github.com>
2023-12-18 17:56:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 import pdfplumber
+import(‘pysqlite3’) import sys sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
 from dotenv import load_dotenv
 import openai
 from langchain.document_loaders import TextLoader
