3efa055d0f8c4e57f4c8b491d71b911a6654d049
Guillaume Olivieri <golivi01@fr-h9xjlf2j4l.emea.media.global.loc>
2023-12-19 12:31:59 +0100

    Add .env.example file

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 
 # Load environment variables
-load_dotenv()
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 #function to remove duplicate content
